61ee165101ba897615f1c4f851b8d47e0d8ffc1a
daniel-city <danielws@umich.edu>
2025-10-01 17:43:31 -0400

    Working on last function

--- PollReader.py
+++ PollReader.py
@@ -127,8 +127,8 @@ class PollReader():
             tuple: A tuple containing the net change for Harris and Trump, in that order.
                    Positive values indicate an increase, negative values indicate a decrease.
         """
-        for index in range(len(self.data_dict))
-        
+        self.data_dict["Harris result"]
+
         pass
 
 
